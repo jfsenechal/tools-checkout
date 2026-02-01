f03b830b4d1dbb3daf5bf63f4797d454e0f175b5
jfsenechal <jfsenechal@gmail.com>
2026-02-01 14:49:40 +0100

    Add `default_user` configuration to `app.php` for environment-based defaults

--- config/app.php
+++ config/app.php
@@ -122,5 +122,9 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
-
+    'default_user' => [
+        'name' => env('DEFAULT_USER_NAME', 'Admin'),
+        'email' => env('DEFAULT_USER_EMAIL', 'admin@example.com'),
+        'password' => env('DEFAULT_USER_PASSWORD', 'password'),
+    ],
 ];
