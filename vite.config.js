58837b6d69a5a620babfa09948cca271e7068f94
Senechal jf <jfsenechal@gmail.com>
2026-03-17 16:49:11 +0100

    update HMR host in Vite config

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     server: {
         host: '0.0.0.0',
         hmr: {
-            host: 'tools.local',
+            host: 'gstock.local',
             protocol: 'wss',
         },
         cors: {
