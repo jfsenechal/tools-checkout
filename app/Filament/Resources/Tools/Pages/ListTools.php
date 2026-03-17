4a2db1fe380a71f724dd33b17f53590889dc55fb
Senechal jf <jfsenechal@gmail.com>
2026-03-17 16:02:24 +0100

    up

--- app/Filament/Resources/Tools/Pages/ListTools.php
+++ app/Filament/Resources/Tools/Pages/ListTools.php
@@ -15,6 +15,11 @@ final class ListTools extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            Actions\Action::make('printQrCodes')
+                ->label('Imprimer QR Codes')
+                ->icon(\Filament\Support\Icons\Heroicon::QrCode)
+                ->color('info')
+                ->url(route('print.qrcodes'), shouldOpenInNewTab: true),
             Actions\CreateAction::make(),
         ];
     }
--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,18 @@ Route::get('/', function () {
 Route::get('/scanner', function () {
     return view('scanner');
 })->name('scanner');
+
+Route::get('/print/qrcodes', function () {
+    $tools = App\Models\Tool::query()
+        ->whereNotNull('qr_code')
+        ->orderBy('name')
+        ->get();
+
+    $workers = App\Models\Worker::query()
+        ->whereNotNull('qr_code')
+        ->orderBy('last_name')
+        ->orderBy('first_name')
+        ->get();
+
+    return view('print-qrcodes', compact('tools', 'workers'));
+})->name('print.qrcodes');
