aa72430b4d0e77c75312715afe461ce281e6f815
jfsenechal <jfsenechal@gmail.com>
2026-03-18 13:58:42 +0100

    up

--- resources/views/scanner.blade.php
+++ resources/views/scanner.blade.php
@@ -428,7 +428,7 @@
                     this.initCameraElements();
                     try {
                         const stream = await navigator.mediaDevices.getUserMedia({
-                            video: { facingMode: 'left' }
+                            video: { facingMode: 'environment' }
                         });
                         this.video.srcObject = stream;
                         await this.video.play();
