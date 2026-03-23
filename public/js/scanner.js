b942fd1f923c3da4630a803428a8b6b10b12a94a
Senechal jf <jfsenechal@gmail.com>
2026-03-23 11:36:46 +0100

    up

--- public/js/scanner.js
+++ public/js/scanner.js
@@ -142,10 +142,30 @@ function scannerApp() {
 
         confirmScan() {
             if (!this.detectedQRData) return;
+            this.playBeep();
             const qrData = this.detectedQRData;
             this.detectedQRData = null;
             this.qrDetected = false;
             this.handleQRCode(qrData);
+        },
+
+        playBeep() {
+            try {
+                const ctx = new (window.AudioContext || window.webkitAudioContext)();
+                const oscillator = ctx.createOscillator();
+                const gain = ctx.createGain();
+                oscillator.connect(gain);
+                gain.connect(ctx.destination);
+                oscillator.type = 'square';
+                oscillator.frequency.setValueAtTime(1800, ctx.currentTime);
+                oscillator.frequency.setValueAtTime(1200, ctx.currentTime + 0.08);
+                gain.gain.setValueAtTime(0.3, ctx.currentTime);
+                gain.gain.exponentialRampToValueAtTime(0.01, ctx.currentTime + 0.15);
+                oscillator.start(ctx.currentTime);
+                oscillator.stop(ctx.currentTime + 0.15);
+            } catch (e) {
+                // Audio not supported, silently ignore
+            }
         },
 
         async handleQRCode(qrData) {
