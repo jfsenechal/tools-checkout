e3c2c4576861b24a4898f48d15e6c898a6fd1107
jfsenechal <jfsenechal@gmail.com>
2026-04-15 10:52:55 +0200

    up

--- clean.sh
+++ clean.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+rm bootstrap/cache/*.php
+rm -fr bootstrap/cache/filament/panels/
+rm -rf bootstrap/cache/blade-icons.php bootstrap/cache/filament/ bootstrap/cache/packages.php bootstrap/cache/services.php
+rm -rf storage/framework/views/*.php
 php artisan config:clear --silent
 php artisan optimize:clear --silent
 php artisan filament:optimize-clear --silent
 rm -f storage/logs/*.log
-php artisan filament:optimize --silent
