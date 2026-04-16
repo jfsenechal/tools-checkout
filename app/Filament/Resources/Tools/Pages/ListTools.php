afccaf1682300777e64803846e3df95e8927e873
Senechal jf <jfsenechal@gmail.com>
2026-04-16 11:14:44 +0200

    up

--- app/Filament/Resources/Tools/Pages/ListTools.php
+++ app/Filament/Resources/Tools/Pages/ListTools.php
@@ -8,10 +8,16 @@ use App\Filament\Resources\Tools\ToolResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Support\Icons\Heroicon;
+use Illuminate\Contracts\Support\Htmlable;
 
 final class ListTools extends ListRecords
 {
     protected static string $resource = ToolResource::class;
+
+    public function getTitle(): string|Htmlable
+    {
+        return $this->getAllTableRecordsCount().' outils';
+    }
 
     protected function getHeaderActions(): array
     {
